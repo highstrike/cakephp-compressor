9c0b4a43b6618b15eb2444dfdf1c56bacf82e42d
Highstrike <highstrike@gmail.com>
2015-05-28 17:45:43 +0300

    commit helper

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# cakephp-compressor
-Helper to compress and combine javascript and css files. Also compresses html output
+# CakePHP Compressor
+Helper to combine and minify javascript and css files. It also minifies html output.
+
+## Dependencies ##
+- PHP 5.4
+- CakePHP 2.6.*
+- Mrclay's Minify!
+
+## Instalation ##
+* Assuming you have at least PHP 5.4 (for the array bracket annotation)
+* Assuming you already have at least CakePHP 2.6.* running (2.6.* is what i've tested with, might also work for previous versions)
+* Run composer (as per **composer.json** included) to install [Mrclay's Minify](https://github.com/mrclay/minify)
+* Assuming you have installed Minify in `your-project/lib/vendor/mrclay` open your `Controller/AppController.php` and include composer's autoload `include(ROOT . DS . 'lib/vendor/autoload.php');`
+
+## Actual Usage ##
+First, open up your `Controller/AppController.php` and add Compressor to your helper variable
+```php
+public $helpers = ['Compressor'];`
+```
+
+#### Configuration ####
+The default configuration of the helper is as follows
+```php
+// default conf
+public $settings = [
+    'html' => [
+        'compression' => true
+    ], 'css' => [
+        'path' => '/cache-css', // without trailing slash
+        'compression' => true
+    ], 'js' => [
+        'path' => '/cache-js', // without trailing slash
+        'compression' => true,
+        'async' => true
+]];
+```
+You can of course overwrite any of the options to when you include it in your `Controller/AppController.php` like so:
+```php
+public $helpers = ['Compressor' => [
+    'html' => [
+        'compression' => false
+    ], 'css' => [
+        'path' => '/my-custom-css-cache-directory',
+    ], 'js' => [
+        'async' => false
+    ]
+]];
+```
+#### Minify HTML output ####
+Including the helper in your `Controller/AppController.php` will automagically minify your HTML output assuming you're in production mode -> `Configure::write('debug', 0);`
+
+#### Combine and Minify css files ####
+The standard way of including css files in your cake project would be to use the HTML helper hence you would have to write something like this `$this->Html->css('site')` where `site.css` is located in `webroot/css/`. If you have multiple css files to include you would probably write something like `$this->Html->css(['site', 'store', 'pagination'])`.
+
+To combine your css files into 1 minified file you need to replace your syntax a little bit:
+```php
+$this->Compressor->style(['site', 'store', 'pagination']);
+$this->Compressor->fetch('style');
+```
+The fetch function actually outputs the generated css file. You should only call this function once in your layout file. You can however call the first function `style` wherever you like, in the layout, in a view file or in an element.
+
+#### Combine and Minify js files ####
+This is almost the same as combining and minifying css files with the exception of syntax change:
+```php
+$this->Compressor->script(['jquery.min', 'frontend']);
+$this->Compressor->fetch('script');
+```
+
+## Auto-Versioning ##
+Upon updating any css or js file in development, a new cache file will be generated on production so you can stop worrying about clients pestering you about not seeing any changes or asking you to explain again how to clear their browsers cache :P
+
+## Important ##
+The Compressor helper does not support combining and minifying off-site resources (such as those from CDNs). Those files are already minifed and you should include them using the standard cake way. For example including jquery from google's CDN should be done like this:
+```php
+$this->Html->css('//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js')
+```
+
+## Debug ##
+Of course, by default the combination and minification of resources works only when you're in production mode -> `Configure::write('debug', 0);`. But if you wish to see if the helper works without going in full fledge production mode you can use a little switch to simulate a live environment. All you have to do is to add a second parameter on the fetch function and set it to true, like so:
+```php
+$this->Compressor->fetch('style', true);
+$this->Compressor->fetch('script', true);
+```
---
+++ View/Helper/CompressorHelper.php
@@ -0,0 +1,211 @@
+<?php
+
+App::uses('AppHelper', 'View/Helper');
+
+/**
+ * Compressor
+ * Will compress HTML
+ * Will combine and compress JS and CSS files
+ *
+ * @author Borg
+ * @version 0.3
+ */
+class CompressorHelper extends AppHelper {
+    // load html helper
+    public $helpers = ['Html'];
+
+    // default conf
+    public $settings = [
+        'html' => [
+            'compression' => true
+        ], 'css' => [
+            'path' => '/cache-css', // without trailing slash
+            'compression' => true
+        ], 'js' => [
+            'path' => '/cache-js', // without trailing slash
+            'compression' => true,
+            'async' => true
+    ]];
+
+    // the view object
+    public $view;
+
+    // container for css and js files
+    private $css = ['intern' => [], 'extern' => []];
+    private $js = ['intern' => [], 'extern' => []];
+
+    // simulate live
+    private $live = false;
+
+    /**
+     * Constructor
+     * @param View $View
+     * @param unknown $settings
+     */
+    public function __construct(View $View, $settings = []) {
+        // set the view object and call parent constructor
+        $this->view = $View;
+        parent::__construct($View, $settings);
+
+        // calculate file system route
+        $this->settings['css']['route'] = rtrim(WWW_ROOT, DS) . str_replace('/', DS, $this->settings['css']['path']);
+        $this->settings['js']['route'] = rtrim(WWW_ROOT, DS) . str_replace('/', DS, $this->settings['js']['path']);
+    }
+
+    /**
+     * HTML compressor
+     * @see Helper::afterLayout()
+     */
+    public function afterLayout($layoutFile) {
+        $this->view->output = $this->_html($this->view->output);
+    }
+
+    /**
+     * Add js files to list
+     * @param array $files
+     */
+    public function script($files = []) {
+        // add each file to group with www_root
+        $group = [];
+        foreach($files as $url)
+            $group[] = rtrim(WWW_ROOT, DS) . str_replace('/', DS, $this->assetUrl($url, array('pathPrefix' => Configure::read('App.jsBaseUrl'), 'ext' => '.js')));
+
+        // array merge
+        $this->js['intern'] = am($group, $this->js['intern']);
+        $this->js['extern'] = am($files, $this->js['extern']);
+    }
+
+    /**
+     * Add css files to list
+     * @param array $files
+     */
+    public function style($files = []) {
+        // add each file to group with www_root
+        $group = [];
+        foreach($files as $url)
+            $group[] = rtrim(WWW_ROOT, DS) . str_replace('/', DS, $this->assetUrl($url, array('pathPrefix' => Configure::read('App.cssBaseUrl'), 'ext' => '.css')));
+
+        // array merge
+        $this->css['intern'] = am($group, $this->css['intern']);
+        $this->css['extern'] = am($files, $this->css['extern']);
+    }
+
+    /**
+     * Fetch either combined css or js
+     * @param string $what style | script
+     * @throws CakeException
+     */
+    public function fetch($what = null, $live = false) {
+        // not supported?
+        if(!in_array($what, ['style', 'script']))
+            throw new CakeException("{$what} not supported");
+
+        // simulate live?
+        $this->live = $live;
+
+        // call private function
+        $function = '_' . $what;
+        $this->$function();
+    }
+
+    /**
+     * Attempt to create the filename for the selected resources
+     * @param string $what js | css
+     * @throws CakeException
+     * @return string
+     */
+    private function filename($what = null) {
+        // not supported?
+        if(!in_array($what, ['css', 'js']))
+            throw new CakeException("{$what} not supported");
+
+        $last = 0;
+        $loop = $this->$what;
+        foreach($loop['intern'] as $res)
+            if(file_exists($res))
+                $last = max($last, filemtime($res));
+
+        return "cache-{$last}-" . md5(serialize($loop['intern'])) . ".{$what}";
+    }
+
+    /**
+     * HTML compressor
+     * @param string $content
+     * @return string
+     */
+    private function _html($content) {
+        // compress?
+        if(Configure::read('debug') == 0 && $this->settings['html']['compression'])
+            $content = trim(\Minify_HTML::minify($content));
+
+        // return
+        return $content;
+    }
+
+    /**
+     * Create the cache file if it doesnt exist
+     * Return the combined css either compressed or not (depending on the setting)
+     */
+    private function _style() {
+        // only compress if we're in production
+        if(Configure::read('debug') == 0 || $this->live == true) {
+            // no cache file? write it
+            $cache = $this->filename('css');
+            if(!file_exists($this->settings['css']['route'] . DS . $cache)) {
+                // get content
+                $content = null;
+                foreach($this->css['intern'] as $file)
+                    $content .= "\n" . file_get_contents($file) . "\n";
+
+                // replace relative paths to absolute paths
+                $content = preg_replace('/(\.\.\/)+/i', Router::url('/', true), $content);
+
+                // compress?
+                if($this->settings['css']['compression'])
+                    $content = trim(\Minify_CSS::minify($content, ['preserveComments' => false]));
+
+                // write to file
+                file_put_contents($this->settings['css']['route'] . DS . $cache, $content);
+            }
+
+            // output with the HTML helper
+            echo $this->Html->css($this->settings['css']['path'] . '/' . $cache);
+
+        // development mode, output separately with the HTML helper
+        } else
+            foreach($this->css['extern'] as $file)
+                echo $this->Html->css($file);
+    }
+
+    /**
+     * Create the cache file if it doesnt exist
+     * Return the combined js either compressed or not (depending on the setting)
+     */
+    private function _script() {
+        // only compress if we're in production
+        if(Configure::read('debug') == 0 || $this->live == true) {
+            // no cache file? write it
+            $cache = $this->filename('js');
+            if(!file_exists($this->settings['js']['route'] . DS . $cache)) {
+                // get content
+                $content = null;
+                foreach($this->js['intern'] as $file)
+                    $content .= "\n" . file_get_contents($file) . "\n";
+
+                // compress?
+                if($this->settings['js']['compression'])
+                    $content = trim(\Minify_JS_ClosureCompiler::minify($content));
+
+                // write to file
+                file_put_contents($this->settings['js']['route'] . DS . $cache, $content);
+            }
+
+            // output with the HTML helper
+            echo $this->Html->script($this->settings['js']['path'] . '/' . $cache, $this->settings['js']['async'] == true ? ['async' => 'async'] : []);
+
+        // development mode, output separately with the HTML helper
+        } else
+            foreach($this->js['extern'] as $file)
+                echo $this->Html->script($file, $this->settings['js']['async'] == true ? ['async' => 'async'] : []);
+    }
+}
